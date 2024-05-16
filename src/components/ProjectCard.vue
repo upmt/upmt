812040acc96fd50f47858a491aea2fdf7dceb07e
Olivier Aubert <contact@olivieraubert.net>
2024-05-16 19:17:15 +0200

    Add projectcard

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-card>
+    <strong>Project {{ project?.name }}</strong>
+    <p>Interview count: {{ interviewCount }}</p>
+    <ul>
+      <li v-for="interview in project?.interviews" :key="interview.id">
+        {{ interview.id }} - {{ interview.name }}
+      </li>
+    </ul>
+    <p>Project count: {{ repo.all().length }}</p>
+    <q-btn @click="addProject">Add project</q-btn>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRepo } from 'pinia-orm'
+import Project from 'src/models/project'
+
+const repo = useRepo(Project)
+
+interface Props {
+    project?: Project | null
+}
+const props = withDefaults(defineProps<Props>(), {
+  project: null
+})
+
+const interviewCount = computed(() => {
+  if (props.project) {
+    return props.project.interviews.length
+  } else {
+    return 0
+  }
+})
+
+const addProject = () => {
+    repo.save({
+        name: "Test project",
+        interviews: [
+            {
+                name: "interview1",
+                color: "black",
+                comment: "",
+                interviewText: "Bla bla bli",
+                participantName: "she"
+            },
+            {
+                name: "interview2",
+                color: "red",
+                comment: "comm",
+                interviewText: "Bla bla bli",
+                participantName: "she"
+            }
+        ]
+    })
+    console.log("inserted project in", repo.piniaStore())
+}
+</script>
