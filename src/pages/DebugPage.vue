13d6f439235eca6802250721e145cb130fc6b65b
Olivier Aubert <contact@olivieraubert.net>
2024-06-07 11:06:14 +0200

    Add v-object-inspector in DebugPage

---
+++ src/pages/DebugPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page padding>
+    <VObjectInspector :data="projects" />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue'
+  import { useProjectStore } from 'stores/projectStore'
+
+  import { VObjectInspector } from 'v-object-inspector'
+  import 'v-object-inspector/dist/style.css'
+
+  defineOptions({
+      name: 'DebugPage'
+  })
+
+  const pstore = useProjectStore()
+  const projects = computed(() => { return pstore.getAllProjects() })
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,15 @@
 import { RouteRecordRaw } from 'vue-router'
+import DebugPage from 'pages/DebugPage.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
+    children: [
+      { path: '', name: 'home', component: () => import('pages/IndexPage.vue') },
+      { path: '', name: 'init', component: () => import('pages/IndexPage.vue'), props: { filename: './OPEVA-G1.upmt' } },
+      { path: 'debug', name: 'debug', component: DebugPage }
+    ]
   },
 
   // Always leave this as last one,
