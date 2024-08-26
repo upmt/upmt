38bc25fb310445a026aaee1b211e167335c8383f
Olivier Aubert <contact@olivieraubert.net>
2024-08-26 10:54:49 +0200

    Add DragElement

---
+++ src/components/DragElement.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dragelement"
+       draggable="true"
+       @dragstart="onDragStart($event)">
+    <slot>
+    </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+      // Data type that is sent (moment, property, etc)
+      type: {
+          type: String,
+          default: ""
+      },
+      // Data to send (most commonly an element id)
+      data: {
+          type: String,
+          default: ""
+      }
+  })
+
+  function onDragStart (event: DragEvent) {
+      if (event.dataTransfer) {
+          event.dataTransfer.setData(`upmt/${props.type}`, props.data)
+      }
+  }
+</script>
+<style>
+  .dragelement {
+    cursor: pointer;
+    display: flex;
+    flex-direction: row;
+  }
+</style>
