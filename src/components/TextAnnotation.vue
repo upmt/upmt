e7d671a07f12e15589dae68caf62d20e37390fba
Olivier Aubert <contact@olivieraubert.net>
2024-09-03 13:03:27 +0200

    Use own AnnotatedText component

--- src/components/TextAnnotation.vue
+++ src/components/TextAnnotation.vue
@@ -1,25 +1,16 @@
 <template>
   <AnnotatedText
     v-if="interview"
-    text="interview.text"
+    :text="interview.text"
     :annotations="annotations"
-    :lines="textLines"
-    :debug="false"
-    :show-labels="false"
-    render="nested"
+    :getSpanClasses="getSpanClasses"
     @click-annotation="onClick"
     />
 </template>
 
 <script setup lang="ts">
   import { computed } from 'vue'
-  import {
-      type Annotation as ViewAnnotation,
-      type Line,
-//      type AnnotationTarget,
-      AnnotatedText
-  } from '@ghentcdh/vue-component-annotated-text'
-  import "@ghentcdh/vue-component-annotated-text/style.css"
+  import AnnotatedText from './AnnotatedText.vue'
   import Annotation from 'stores/models/annotation'
   import Interview from 'stores/models/interview'
 
@@ -27,52 +18,18 @@
       interview: { type: Interview, default: null }
   })
 
-  const textToLines = (text: string): Line[] => {
-      const regLineNumber = /^([0-9/]+[a-z]?)\./g
-      let lineStart = 0
-      let lineEnd = 0
-      let gutter = ""
-
-      // split text into lines
-      const lines = text.split("\n")
-      const lineObjects: Line[] = []
-
-      // split lines into line number, text, start and end
-      for (let i = 0; i < lines.length; i++) {
-          lineEnd = lineStart + (lines[i].length - 1)
-          const match = lines[i].match(regLineNumber)
-          gutter = match?.length ? match[0] : ""
-          lineObjects[i] = {
-              gutter,
-              text: lines[i].replace(regLineNumber, ""),
-              start: lineStart + gutter.length,
-              end: lineEnd
-          }
-          lineStart = lineEnd + 2
-
-          // empty lines:
-          lineObjects[i].end = Math.max(lineObjects[i].end || 0, lineObjects[i].start || 0)
-      }
-
-      return lineObjects;
-  }
-
-  const textLines = computed(() => textToLines(props.interview.text))
-
   const onClick = (annotation: Annotation) => {
       console.log(annotation)
   }
 
   /*
     export const annotationsTest = [
-  {
-    start: 5,
-    end: 9,
-    class: "annotation annotation--color-1",
-    target: "span",
-    metadata: { id: 1 },
-    label: "typo",
-    },
+{
+    start: 0,
+    length: 7,
+    id: 1,
+    class: "process"
+}
   */
   const annotation2class = (a: Annotation) => {
       const mapping: Record<string, string> = {
@@ -81,25 +38,47 @@
           '#ff9797': '1',
           '#ffdc97': '3'
       }
-      return `annotation--color-${mapping[a.color] ?? '9'}`
+      return `category${mapping[a.color] ?? '9'}`
   }
 
-  const annotations = computed((): ViewAnnotation[] => {
+  const annotations = computed(() => {
       return props.interview.annotations.map(a => {
           return {
+              id: a.id,
               start: a.startIndex,
-              end: a.endIndex,
-              target: "span",
-              label: a.color,
-              class: `annotation ${annotation2class(a)}`
+              length: a.endIndex - a.startIndex,
+              color: a.color,
+              class: annotation2class(a),
+              item: a
           }
       })
   })
+
+  function getSpanClasses (span: any) {
+      const classes = [ ...new Set(span.annotations.map((a: Annotation) => annotation2class(a))) ]
+      return classes.join(" ")
+  }
 </script>
 
 <style>
   .annotated-text {
       display: flex;
       flex-direction: column;
+  }
+  .category1 {
+      background-color: #ff9797;
+  }
+  .category2 {
+      background-color: #7084b0;
+  }
+  .category3 {
+      background-color: #ffdc97;
+  }
+  .category4 {
+      background-color: #7bcf7b;
+  }
+  /* For undefined categories */
+  .category9 {
+      background-color: yellow;
   }
 </style>
