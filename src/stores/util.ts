ed86d9448cddae53752b28c9ddab4cab89ef6bec
Olivier Aubert <contact@olivieraubert.net>
2024-09-18 15:01:07 +0200

    Remove space around ellipsis

--- src/stores/util.ts
+++ src/stores/util.ts
@@ -4,7 +4,7 @@ function ellipsize (text: string, limit: number = SHORT_TEXT_LIMIT): string {
   if (text.length < limit) {
     return text
   } else {
-    return `${text.slice(0, limit / 2)}\u{00a0}\u{22ef}\u{00a0}${text.slice(-limit / 2)}`
+    return `${text.slice(0, limit / 2)}\u{22ef}${text.slice(-limit / 2)}`
   }
 }
 
