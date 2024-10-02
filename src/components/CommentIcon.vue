04b384483574de4c064b5fc79ac2f68fee7df9f2
Olivier Aubert <contact@olivieraubert.net>
2024-10-02 09:36:50 +0200

    Add CommentIcon component

---
+++ src/components/CommentIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-icon
+    size="xs"
+    flat
+    round
+    :class="commentClass"
+    name="mdi-chat-outline">
+    <q-tooltip
+      v-if="comment">
+      <span class="moment-comment">{{ comment }}</span>
+    </q-tooltip>
+  </q-icon>
+</template>
+
+<script setup lang="ts">
+
+  import { computed } from 'vue'
+
+  const props = defineProps({
+      comment: { type: String, default: "" }
+  })
+
+  const commentClass = computed(() => {
+      return props.comment ? [ "comment" ] : [ "comment", "comment-is-empty" ]
+  })
+</script>
+
+<style scoped>
+  .comment-is-empty {
+      opacity: .3;
+  }
+</style>
