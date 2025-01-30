16f695284345c5ebe0bbbd1a75d2518bcd2eab93
Olivier Aubert <contact@olivieraubert.net>
2025-01-30 18:19:20 +0100

    Add Ruptur2-example

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@
       istore.setUsername(localStorage.getItem('upmtUsername') ?? "anonymous")
       store.loadProject('./examples/example.upmt');
       store.loadProject('./examples/ruptur-example.upmt');
+      store.loadProject('./examples/ruptur2-example.upmt');
       (window as any).store = store;
       (window as any).repo = store.getRepo();
       console.log("store = ", store);
