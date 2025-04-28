041a934396452d910ca2b4d0d9baeb17f82dae4a
Olivier Aubert <contact@olivieraubert.net>
2025-04-28 11:30:41 +0200

    Display getgenericsynchronicmodels output in debug page

--- src/pages/DebugPage.vue
+++ src/pages/DebugPage.vue
@@ -1,12 +1,14 @@
 <template>
   <q-page padding>
     <VObjectInspector :data="projects" />
+    <VObjectInspector :expandLevel="2" :expandPaths="['$.*.children']" :data="genericmodels" />
   </q-page>
 </template>
 
 <script setup lang="ts">
   import { computed } from 'vue'
   import { useProjectStore } from 'stores/projectStore'
+  import { useInterfaceStore } from 'stores/interface'
 
   import { VObjectInspector } from 'v-object-inspector'
   import 'v-object-inspector/dist/style.css'
@@ -15,6 +17,8 @@
       name: 'DebugPage'
   })
 
-  const pstore = useProjectStore()
-  const projects = computed(() => { return pstore.getAllProjects() })
+  const store = useProjectStore()
+  const istore = useInterfaceStore()
+  const projects = computed(() => { return store.getAllProjects() })
+  const genericmodels = computed(() => store.getGenericSynchronicModels(istore.getProjectId()))
 </script>
