36a574066ee8476c44fbdd6679028717a9392e48
Olivier Aubert <contact@olivieraubert.net>
2025-12-17 15:35:15 +0100

    Do not paginate descriptemtable

--- src/components/DescriptemTable.vue
+++ src/components/DescriptemTable.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="descriptems">
     <q-table
+      style="height: 80vh"
+      dense
       flat bordered
       title="Descriptems"
       :rows="descriptems"
       :columns="columns"
       color="primary"
       row-key="id"
+      v-model:pagination="pagination"
+      :rows-per-page-options="[0]"
     >
       <template v-slot:top-right>
         <q-btn
@@ -23,7 +27,7 @@
 
 <script setup lang="ts">
 
-  import { computed } from 'vue'
+  import { computed, ref } from 'vue'
 
   import { useProjectStore } from 'stores/projectStore'
 
@@ -36,8 +40,14 @@
   const descriptems = computed(() => store.getDescriptemsByProject(props.projectId ?? ""))
 
   const columns = [
+      { name: 'interview',
+        label: 'Interview',
+        field: (row: any) => row.interview.name,
+        sortable: true },
       { name: 'text',
         label: 'Text',
+        classes: 'column-small',
+        headerClasses: 'column-small',
         align: "left" as const,
         field: (row: any) => row.text,
         sortable: true },
@@ -48,10 +58,6 @@
       { name: 'end',
         label: 'End',
         field: (row: any) => row.endIndex,
-        sortable: true },
-      { name: 'interview',
-        label: 'Interview',
-        field: (row: any) => row.interview.name,
         sortable: true },
       { name: 'moment',
         label: 'Moment',
@@ -70,10 +76,26 @@
       { name: 'modified', label: 'Modified', field: 'modified', sortable: true },
       ]
 
+  const pagination = ref({
+      rowsPerPage: 0
+  })
+
   function exportTable () {
       console.log(descriptems.value)
   }
 </script>
 
-<style scoped>
+<style>
+  .column-large {
+    width: 400px !important;
+    overflow-x: hidden;
+  }
+  .column-medium {
+    width: 300px !important;
+    overflow-x: hidden;
+  }
+  .column-small {
+      width: 200px !important;
+    overflow-x: hidden;
+  }
 </style>
