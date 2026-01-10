7e3de4c74fe9e15fa9c58a814492fe7434a124d0
Olivier Aubert <contact@olivieraubert.net>
2026-01-10 20:09:38 +0100

    Define CustomExpansionItem element

---
+++ src/components/CustomExpansionItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div :class="itemClass">
+    <div class="expansion-header row items-center">
+      <q-icon
+        class="expansion-icon"
+        :class="expandIconClass"
+        @click.stop="isExpanded = !isExpanded"
+        :name="isExpanded ? expandedIcon : expandIcon" />
+      <slot name="header"></slot>
+    </div>
+    <div class="expansion-body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { computed, ref } from 'vue'
+
+  interface Props {
+      expandIcon?: string,
+      expandedIcon?: string,
+      expandIconClass?: string
+  }
+  withDefaults(defineProps<Props>(), {
+      expandIcon: "mdi-plus",
+      expandedIcon: "mdi-minus",
+      expandIconClass: ""
+  })
+
+  const isExpanded = ref(true)
+
+  const itemClass = computed(() => {
+      return isExpanded.value ? "expansion-item column expanded" : "expansion-item column collapsed"
+  })
+</script>
+<style scoped>
+.collapsed .expansion-body {
+    display: none;
+}
+.expansion-body {
+    margin-left: 10px;
+}
+.expansion-header {
+    border: 1px solid transparent;
+}
+.expansion-header:hover {
+    border: 1px dashed #ccc;
+}
+.expansion-icon {
+    cursor: pointer;
+}
+</style>
