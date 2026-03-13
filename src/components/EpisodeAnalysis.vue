6cdb7169d4529a76f5999b439e4d427a58fc1e2c
Olivier Aubert <contact@olivieraubert.net>
2026-03-13 18:56:06 +0100

    feat(gui): Display depth marker labels

--- src/components/EpisodeAnalysis.vue
+++ src/components/EpisodeAnalysis.vue
@@ -14,6 +14,7 @@
                 class="col-4"
                 snap
                 markers
+                marker-labels
                 :min="0"
                 :max="4"
                 :step="1"
